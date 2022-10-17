77a4c1773976c82a753609239629a5087fd58bbe
Thiago Fabricio <thiago.fabricio123@gmail.com>
2022-10-17 19:44:10 -0300

    Criacao dos Teste do Model de Turma

--- test/models/turma_test.rb
+++ test/models/turma_test.rb
@@ -1,7 +1,29 @@
 require "test_helper"
 
 class TurmaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "criar turma corretamente" do
+    users = User.new
+    users.nome = "Teste"
+    users.email = "teste@teste.com"
+    users.password = "123456"
+    users.password_confirmation = "123456"
+
+    turma = Turma.new
+    turma.letra_turma = "A"
+    turma.ano = "1"
+    turma.ano_letivo = "2021"
+    turma.user = users
+
+    assert turma.save
+  end
+
+  test "criar turma sem user" do
+    turma = Turma.new
+    turma.letra_turma = "A"
+    turma.ano = "1"
+    turma.ano_letivo = "2021"
+
+    assert_not turma.save
+  end
 end
