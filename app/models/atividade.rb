5238c7c49e21b21d61faa886d16801a1e200d460
Thiago Fabricio <thiago.fabricio123@gmail.com>
2022-10-17 18:30:25 -0300

    Criacao dos Validates de Atividade

--- app/models/atividade.rb
+++ app/models/atividade.rb
@@ -1,4 +1,8 @@
 class Atividade < ApplicationRecord
 
   belongs_to :disciplina
+
+  validates :nome, presence: true, length: { maximum: 50 }
+  validates :descricao, presence: true, length: { maximum: 255 }
+  validates :disciplina_id, presence: true
 end
