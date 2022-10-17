2b2a8f8a5f7c0ad88b5265b99b4dc369fd6604fe
Thiago Fabricio <thiago.fabricio123@gmail.com>
2022-10-17 19:59:02 -0300

    Criacao dos Teste de Model de Atividade

--- test/models/atividade_test.rb
+++ test/models/atividade_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 class AtividadeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+    test "criar atividade sem disciplina" do
+        atividade = Atividade.new
+        atividade.titulo = "Teste"
+        atividade.descricao = "Teste"
+        atividade.bimestre = "1"
+        atividade.data = "2021-01-01"
+
+        assert_not atividade.save
+    end
+
+    test "criar atividade sem titulo" do
+        disciplinas = Disciplina.new
+        disciplinas.nome = "Matematica"
+        disciplinas.ano_letivo = "2021"
+
+        atividade = Atividade.new
+        atividade.descricao = "Teste"
+        atividade.bimestre = "1"
+        atividade.data = "2021-01-01"
+        atividade.disciplina = disciplinas
+
+        assert_not atividade.save
+    end
 end
