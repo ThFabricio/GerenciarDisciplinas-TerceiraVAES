646ababe5b9ffc20890295fc8b4e3675c5cea451
Thiago Fabricio <thiago.fabricio123@gmail.com>
2022-10-17 19:50:45 -0300

    Criacao dos Teste de Model de Aluno

--- test/models/aluno_test.rb
+++ test/models/aluno_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
 class AlunoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "criar aluno sem nome" do
+    turmas = Turma.new
+    turmas.letra_turma = "A"
+    turmas.ano = "1"
+    turmas.ano_letivo = "2021"
+
+    aluno = Aluno.new
+    aluno.turma = turmas
+
+    assert_not aluno.save
+  end
+
+  test "criar aluno sem turma" do
+    aluno = Aluno.new
+    aluno.nome = "Teste"
+
+    assert_not aluno.save
+  end
+
 end
