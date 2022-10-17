a770b27beaa9fc95a568b34a1d29b5c90edfd705
Thiago Fabricio <thiago.fabricio123@gmail.com>
2022-10-17 17:20:43 -0300

    Adicionamento das Foreignkey de User e Turma em Disciplina

--- db/migrate/20221017200035_create_disciplinas.rb
+++ db/migrate/20221017200035_create_disciplinas.rb
@@ -3,6 +3,9 @@ class CreateDisciplinas < ActiveRecord::Migration[7.0]
     create_table :disciplinas do |t|
       t.string :nome
 
+      t.references :user, null: false, foreign_key: true
+      t.references :turma, null: false, foreign_key: true
+
       t.timestamps
     end
   end
