17635211041999b2174f69a662b270487e44a164
Thiago Fabricio <thiago.fabricio123@gmail.com>
2022-10-17 18:24:56 -0300

    Ajsutes no Validate no Model de Discipilna

--- app/models/disciplina.rb
+++ app/models/disciplina.rb
@@ -2,9 +2,10 @@ class Disciplina < ApplicationRecord
 
   belongs_to :user
   belongs_to :turma
+  has_many :atividades, dependent: :destroy
 
   validates :nome, presence: true, length: { minimum: 3, maximum: 50 }
-  validates :ano_letivo, presence: true, length: { minimum: 4, maximum: 4 }, numericality: { only_integer: true }, inclusion: { in: 2000..Date.today.year }, uniqueness: { scope: :turma_id, message: "já existe uma disciplina com esse nome para essa turma" }
+  validates :ano_letivo, presence: true, length: { minimum: 4, maximum: 4 }, numericality: { only_integer: true }, uniqueness: { scope: :turma_id, message: "já existe uma disciplina com esse nome para essa turma" }
   validates :user_id, presence: true
   validates :turma_id, presence: true
 end
