d598d0e6c9b1047d381b55bddf5c73f661c690d5
Thiago Fabricio <thiago.fabricio123@gmail.com>
2022-10-17 16:58:26 -0300

    Criacao dos Validates de Turma

--- app/models/turma.rb
+++ app/models/turma.rb
@@ -2,5 +2,9 @@ class Turma < ApplicationRecord
 
   belongs_to :user
 
+  validates :letra_turma, presence: true, length: { maximum: 1 }, format: { with: /\A[a-zA-Z]+\z/, message: "Letra da turma inválida" }
+  validates :ano, presence: true, length: { maximum: 4 }, format: { with: /\A[0-9]+\z/, message: "Ano inválido" }
+  validates :ano_letivo, presence: true, length: { maximum: 4 }, format: { with: /\A[0-9]+\z/, message: "Ano letivo inválido" }
+
 end
 
