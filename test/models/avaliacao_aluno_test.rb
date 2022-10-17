e1e2ae7cddf267700f1263c83d20593756cad2ea
Thiago Fabricio <thiago.fabricio123@gmail.com>
2022-10-17 20:08:29 -0300

    Criacao dos Teste de Model de Avaliacao Aluno

--- test/models/avaliacao_aluno_test.rb
+++ test/models/avaliacao_aluno_test.rb
@@ -1,7 +1,25 @@
 require "test_helper"
 
 class AvaliacaoAlunoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "criar avaliacao aluno sem aluno" do
+    avaliacao_aluno = AvaliacaoAluno.new
+    avaliacao_aluno.pontos = "10"
+    avaliacao_aluno.observacoes = "Teste"
+    avaliacao_aluno.atividade = Atividade.first
+
+    assert_not avaliacao_aluno.save
+  end
+
+  test "criar avaliacao aluno sem atividade" do
+    alunos = Aluno.new
+    alunos.nome = "Teste"
+
+    avaliacao_aluno = AvaliacaoAluno.new
+    avaliacao_aluno.pontos = "10"
+    avaliacao_aluno.observacoes = "Teste"
+    avaliacao_aluno.aluno = alunos
+
+    assert_not avaliacao_aluno.save
+  end
 end
