6ce62c524e692d327b989bcf7a60c3d6d595b71c
Thiago Fabricio <thiago.fabricio123@gmail.com>
2022-10-17 16:09:50 -0300

    Criacao dos Validates de User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nome, presence: true, length: { maximum: 50 }
+  validates :email , presence: true, length: { maximum: 100, minimum: 5 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "E-mail inválido"}, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 end
