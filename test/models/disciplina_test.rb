94f2ac48ba40af02d81739dec12e6a49ab473058
Thiago Fabricio <thiago.fabricio123@gmail.com>
2022-10-17 19:48:50 -0300

    Criaco dos Teste do Model de Turma

--- test/models/disciplina_test.rb
+++ test/models/disciplina_test.rb
@@ -1,7 +1,33 @@
 require "test_helper"
 
 class DisciplinaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "criar disciplina sem turma" do
+    users = User.new
+    users.nome = "Teste"
+    users.email = "teste@gmail.com"
+    users.password = "123456"
+    users.password_confirmation = "123456"
+
+    disciplina = Disciplina.new
+    disciplina.nome = "Matematica"
+    disciplina.ano_letivo = "2021"
+    disciplina.user = users
+
+    assert_not disciplina.save
+  end
+
+  test "criar disciplina sem user" do
+    turmas = Turma.new
+    turmas.letra_turma = "A"
+    turmas.ano = "1"
+    turmas.ano_letivo = "2021"
+
+    disciplina = Disciplina.new
+    disciplina.nome = "Matematica"
+    disciplina.ano_letivo = "2021"
+    disciplina.turma = turmas
+
+    assert_not disciplina.save
+  end
 end
