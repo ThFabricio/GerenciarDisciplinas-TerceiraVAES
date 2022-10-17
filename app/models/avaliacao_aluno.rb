2039d5d7cd2054a744434ccd7b0fc140aa4ed3fc
Thiago Fabricio <thiago.fabricio123@gmail.com>
2022-10-17 18:59:41 -0300

    Validates do Model de Avaliacao Aluno

--- app/models/avaliacao_aluno.rb
+++ app/models/avaliacao_aluno.rb
@@ -2,4 +2,9 @@ class AvaliacaoAluno < ApplicationRecord
 
   belongs_to :aluno
   belongs_to :atividade
+
+  validates :pontos, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
+  validates :observacoes, presence: true, length: { maximum: 255 }
+  validates :aluno_id, presence: true
+  validates :atividade_id, presence: true
 end
