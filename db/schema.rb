392ccfed9bbb9609eaab4a3989567c659fbebf22
Thiago Fabricio <thiago.fabricio123@gmail.com>
2022-10-17 17:47:47 -0300

    Adicionamento do link para volta a Homepage, nas View de Aluno

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_17_200035) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_17_204421) do
+  create_table "alunos", force: :cascade do |t|
+    t.string "nome"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "disciplinas", force: :cascade do |t|
     t.string "nome"
     t.string "ano_letivo"
