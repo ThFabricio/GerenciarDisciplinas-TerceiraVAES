36abfb768d74408d99b52f9a9c5598025353c2f7
Thiago Fabricio <thiago.fabricio123@gmail.com>
2022-10-17 17:40:52 -0300

    Criacao dos validates do Model de Disciplina

--- app/models/disciplina.rb
+++ app/models/disciplina.rb
@@ -2,4 +2,9 @@ class Disciplina < ApplicationRecord
 
   belongs_to :user
   belongs_to :turma
+
+  validates :nome, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :ano_letivo, presence: true, length: { minimum: 4, maximum: 4 }, numericality: { only_integer: true }, inclusion: { in: 2000..Date.today.year }, uniqueness: { scope: :turma_id, message: "já existe uma disciplina com esse nome para essa turma" }
+  validates :user_id, presence: true
+  validates :turma_id, presence: true
 end
