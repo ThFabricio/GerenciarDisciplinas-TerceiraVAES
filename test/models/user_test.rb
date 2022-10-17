012971c38ad15bd8649d3b3730f79f7a72d1457c
Thiago Fabricio <thiago.fabricio123@gmail.com>
2022-10-17 19:39:43 -0300

    Criacao dos Testes do Model de User

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "criar usuario corretamente" do
+    user = User.new
+    user.nome = "Teste"
+    user.email = "teste@teste.com"
+    user.password = "123456"
+    user.password_confirmation = "123456"
+
+    assert user.save
+  end
+
+  test "criar usuario sem nome" do
+    user = User.new
+    user.email = "teste@teste.com"
+    user.password = "123456"
+    user.password_confirmation = "123456"
+
+    assert_not user.save
+  end
 end
