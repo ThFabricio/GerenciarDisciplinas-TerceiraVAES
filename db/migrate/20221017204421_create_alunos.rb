1ea5cdf3e04f6413e38954a41282fa141ef206b6
Thiago Fabricio <thiago.fabricio123@gmail.com>
2022-10-17 17:49:33 -0300

    Adicionamento da ForeignKey de Turma em Aluno

--- db/migrate/20221017204421_create_alunos.rb
+++ db/migrate/20221017204421_create_alunos.rb
@@ -2,6 +2,7 @@ class CreateAlunos < ActiveRecord::Migration[7.0]
   def change
     create_table :alunos do |t|
       t.string :nome
+      t.references :turma, null: false, foreign_key: true
 
       t.timestamps
     end
