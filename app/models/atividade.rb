8a7c84db173ba3e88c542983a50137906fe36e5d
Thiago Fabricio <thiago.fabricio123@gmail.com>
2022-10-17 19:57:33 -0300

    Ajustes nos Validates do Model de Atividade

--- app/models/atividade.rb
+++ app/models/atividade.rb
@@ -3,7 +3,9 @@ class Atividade < ApplicationRecord
   belongs_to :disciplina
   has_many :avaliacao_atividades, dependent: :destroy
 
-  validates :nome, presence: true, length: { maximum: 50 }
-  validates :descricao, presence: true, length: { maximum: 255 }
+  validates :titulo, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :descricao, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :bimestre, presence: true, length: { minimum: 1, maximum: 1 }, numericality: { only_integer: true }, inclusion: { in: 1..4 }
+  validates :data, presence: true
   validates :disciplina_id, presence: true
 end
